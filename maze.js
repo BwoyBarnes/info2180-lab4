6719780d67cd202415353357aa636c4b699b77f7
BwoyBarnes <odanebarnes@outlook.com>
2016-10-05 10:10:49 -0500

    Exercise 4: Restartable maze

--- maze.js
+++ maze.js
@@ -1,9 +1,12 @@
 var boundaries;
 var end;
+var start;
 window.onload = function() {
-    //var boundary1 = document.getElementById("boundary1");
     boundaries = document.querySelectorAll(".boundary");
     end = document.getElementById("end");
+    start = document.getElementById("start");
+
+    start.onclick = resetGame;
     end.onmouseover = CheckWin;
     for (var i = 0; i < boundaries.length; i++) {
 		boundaries[i].onmouseover = overBounds;
@@ -15,6 +18,7 @@ window.onload = function() {
 	//myElement.style.backgroundColor = "red";
 	//boundary1.style.backgroundColor = div.youlose.backgroundColor;
 	//document.getElementById("boundary1").style.backgroundColor = "red";
+
 function overBounds(){
 		if(!hasClass(boundaries[0], "youlose")) // Checks if the youlose class has been applied to at least one of the divs already
 		{
@@ -24,8 +28,6 @@ function overBounds(){
 		}
 	}
 
-
-
 function CheckWin(){
 	if(!hasClass(boundaries[0], "youlose")){
 		alert("You Win!!!");
@@ -33,6 +35,13 @@ function CheckWin(){
 
 }
 
+function resetGame(){
+	for (var i = 0; i < boundaries.length; i++) {
+		boundaries[i].className = "boundary";
+	}
+	
+
+}
 
 //Used to check if a div element already has the youlose colour applied to it to prevent
 //the onmouseover event from adding multiple instances of the class the the element
