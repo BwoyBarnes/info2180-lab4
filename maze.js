a1eb0f0178d3b2ae27bed81c43546cc4699b51fe
BwoyBarnes <odanebarnes@outlook.com>
2016-10-05 09:55:59 -0500

    Exercise 3: Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,7 +1,10 @@
 var boundaries;
+var end;
 window.onload = function() {
     //var boundary1 = document.getElementById("boundary1");
     boundaries = document.querySelectorAll(".boundary");
+    end = document.getElementById("end");
+    end.onmouseover = CheckWin;
     for (var i = 0; i < boundaries.length; i++) {
 		boundaries[i].onmouseover = overBounds;
 	}
@@ -12,8 +15,7 @@ window.onload = function() {
 	//myElement.style.backgroundColor = "red";
 	//boundary1.style.backgroundColor = div.youlose.backgroundColor;
 	//document.getElementById("boundary1").style.backgroundColor = "red";
-
-	function overBounds(){
+function overBounds(){
 		if(!hasClass(boundaries[0], "youlose")) // Checks if the youlose class has been applied to at least one of the divs already
 		{
 			for (var i = 0; i < boundaries.length; i++) {
@@ -22,6 +24,14 @@ window.onload = function() {
 		}
 	}
 
+
+
+function CheckWin(){
+	if(!hasClass(boundaries[0], "youlose")){
+		alert("You Win!!!");
+	}
+
+}
 
 
 //Used to check if a div element already has the youlose colour applied to it to prevent
