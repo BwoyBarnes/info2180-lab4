80019a1eeb1f9c4dd82d4be2396e7d982a7b6db9
BwoyBarnes <odanebarnes@outlook.com>
2016-10-05 09:43:36 -0500

    Exercise 2: All boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -1,5 +1,11 @@
+var boundaries;
 window.onload = function() {
-    var boundary1 = document.getElementById("boundary1");
+    //var boundary1 = document.getElementById("boundary1");
+    boundaries = document.querySelectorAll(".boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+		boundaries[i].onmouseover = overBounds;
+	}
+}
 
     //Test Code
     //var myElement = document.getElementById("boundary1");
@@ -7,17 +13,20 @@ window.onload = function() {
 	//boundary1.style.backgroundColor = div.youlose.backgroundColor;
 	//document.getElementById("boundary1").style.backgroundColor = "red";
 
-	boundary1.onmouseover=function(){
-		if(!hasClass(boundary1, "youlose")){
-			boundary1.className += " youlose";
+	function overBounds(){
+		if(!hasClass(boundaries[0], "youlose")) // Checks if the youlose class has been applied to at least one of the divs already
+		{
+			for (var i = 0; i < boundaries.length; i++) {
+			boundaries[i].className += " youlose";
+			}
 		}
-		
 	}
-	
-}
+
+
 
 //Used to check if a div element already has the youlose colour applied to it to prevent
 //the onmouseover event from adding multiple instances of the class the the element
 function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
+
